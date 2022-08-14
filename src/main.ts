63f467c3f65a9a268b30eba4d4919898b838b805
James Dean <kreebog@gmail.com>
2022-08-14 13:35:17 -0400

    hello-world game server built

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+// load local config from .env file
+dotenv.config()
+
+// express module used for basic HTTP communication
+const app: Express = express()
+
+// load some environment variables
+const name: string = process.env.NAME === undefined ? 'CHECK ENV VAR "NAME"' : process.env.NAME
+const version: string = process.env.VERSION === undefined ? 'CHECK ENV VAR "VERSION"' : process.env.VERSION
+const port: number = Number.parseInt(process.env.PORT === undefined ? '8080' : process.env.PORT)
+
+app.listen(port, () => {
+  console.log(`${name} v${version} listening on ${port}`)
+})
+
+app.get('/', (req: Request, res: Response) => {
+  const url: string = req.url
+  console.log(`Request URL: ${url}`)
+  res.send('Hello from Junkbot Game Server')
+})
