99415c48bc9dd1a668c2e453f6d6fcb4450f7274
James Dean <kreebog@gmail.com>
2022-08-15 14:12:39 -0400

    trying harder to break sonar

--- src/main copy.ts
+++ src/main copy.ts
@@ -7,17 +7,47 @@ dotenv.config()
 // express module used for basic HTTP communication
 const app: Express = express()
 
-const username = 'myname'
-const password = 'whatever'
-
 // load some environment variables
 const name: string = process.env.NAME === undefined ? 'CHECK ENV VAR "NAME"' : process.env.NAME
 const version: string = process.env.VERSION === undefined ? 'CHECK ENV VAR "VERSION"' : process.env.VERSION
 const port: number = Number.parseInt(process.env.PORT === undefined ? '8080' : process.env.PORT)
 
+function fakeLogin (userName: string, password: string): boolean {
+  console.log(`${userName}:${password}`)
+  return (userName === 'myname' && password === 'whatever')
+}
+
 app.listen(port, () => {
   console.log(`${name} v${version} listening on ${port}`)
-  console.log(`${username}:${password}`)
+
+  const username = 'myname'
+  const password = 'whatever'
+
+  if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 1.. ')
+  } else if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 2.. ')
+  } else if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 3.. ')
+  } else if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 4.. ')
+  } else if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 4.. ')
+  } else if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 4.. ')
+  } else if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 4.. ')
+  } else if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 4.. ')
+  } else if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 4.. ')
+  } else if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 4.. ')
+  } else if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 4.. ')
+  } else if (fakeLogin(username, password)) {
+    console.log('Nesting ifs 4.. ')
+  }
 })
 
 app.get('/', (req: Request, res: Response) => {
