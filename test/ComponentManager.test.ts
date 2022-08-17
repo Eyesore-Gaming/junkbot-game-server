212ff0d7ab675dc156c221847f637e0d26469538
James Dean <kreebog@gmail.com>
2022-08-16 21:47:58 -0400

    fixing someone's linter errors

--- test/ComponentManager.test.ts
+++ test/ComponentManager.test.ts
@@ -3,7 +3,7 @@ import { TransformComponent } from '../src/TransformComponent'
 import { TranslationComponent } from '../src/TranslationComponent'
 
 const data = { position: { x: 1, y: 2, z: 3 }, rotation: { a: 4, b: 5, c: 6, d: 7 }, scale: { l: 8, w: 9, h: 10 } }
-const newData =  { position: { x: 3, y: 4, z: 5 }, rotation: { a: 4, b: 5, c: 6, d: 7 }, scale: { l: 8, w: 9, h: 10 } }
+const newData = { position: { x: 3, y: 4, z: 5 }, rotation: { a: 4, b: 5, c: 6, d: 7 }, scale: { l: 8, w: 9, h: 10 } }
 
 let componentManager = new ComponentManager()
 let transformComponent = new TransformComponent()
@@ -11,7 +11,7 @@ let transformComponent = new TransformComponent()
 beforeEach(() => {
   componentManager = new ComponentManager()
   transformComponent = new TransformComponent()
-});
+})
 
 test('Should subscribe an entity', () => {
   componentManager.subscribeEntity(100, transformComponent, data)
@@ -22,7 +22,7 @@ test('Should subscribe an entity', () => {
 test('Should not unsubscribe any entity, as an unused entity id was provided', () => {
   componentManager.subscribeEntity(0, transformComponent, data)
   componentManager.unsubscribeEntity(1, transformComponent)
-  let query = componentManager.query(transformComponent)
+  const query = componentManager.query(transformComponent)
   expect(query.length).toBe(1)
 })
 
@@ -37,7 +37,7 @@ test('Should unsubscribe an entity, with zero remaining', () => {
 })
 
 test('Subscribing to an entity that is already subscribed should update the existing entity', () => {
-  let id = 2
+  const id = 2
   componentManager.subscribeEntity(id, transformComponent, data)
   expect(transformComponent.sparseArray[id]?.position.x).toBe(data.position.x)
   expect(transformComponent.sparseArray[id]?.position.y).toBe(data.position.y)
@@ -52,7 +52,7 @@ test('return only entitiy IDs that have every given component type', () => {
   componentManager.subscribeEntity(0, transformComponent, data)
   componentManager.subscribeEntity(1, transformComponent, data)
   componentManager.subscribeEntity(1, translationComponent, { speed: 5, direction: 180 })
-  //Should return only entities with both a transformComponent and translationComponent registered to them.
+  // Should return only entities with both a transformComponent and translationComponent registered to them.
   const query = componentManager.query(transformComponent, translationComponent)
   expect(query.length).toBe(1)
   expect(query[0]).toBe(1)
